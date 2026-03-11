7e2fa27d05bffab81561589104c017f13fe6d1d9
maxonchickdev <timetotime828@gmail.com>
2026-03-11 10:36:33 +0200

    chore(root): remove unused configuration files and scripts
    - Deleted .prettierignore, eslint.config.ts, and prettier.config.ts as they are no longer needed.
    - Updated package.json to remove linting scripts related to ESLint and Prettier.
    - Cleaned up package-lock.json to reflect the removal of unnecessary dependencies.

--- knip.config.ts
+++ knip.config.ts
@@ -3,7 +3,7 @@ import { type KnipConfig } from "knip";
 const config: KnipConfig = {
   $schema: "https://unpkg.com/knip@5/schema.json",
   project: ["src/**/*.ts"],
-  ignoreDependencies: ["tsconfig-paths", "@commitlint/cli", "pg"],
+  ignoreDependencies: ["tsconfig-paths", "pg"],
   ignoreMembers: ["DEVELOPMENT", "STAGING", "P2002", "P2025"],
   ignore: ["src/generated/**"],
 };
