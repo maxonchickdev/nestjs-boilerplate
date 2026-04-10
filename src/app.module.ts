229538f638ebec549799b435c904ae6fc8f92107
maxonchickdev <timetotime828@gmail.com>
2026-04-10 13:06:24 +0300

    feat(root): refactor module structure by removing core module and updating app module imports
    - Refactor module structure by removing core module and updating app module imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
-import { CoreModule } from "./core/core.module.js";
+import { ConfigModule } from "./core/config/config.module.js";
+import { HealthChecksModule } from "./core/health-checks/health-checks.module.js";
+import { JwtModule } from "./core/jwt/jwt.module.js";
+import { PrismaModule } from "./core/prisma/prisma.module.js";
+import { RateLimitModule } from "./core/rate-limit/rate-limit.module.js";
+import { RedisModule } from "./core/redis/redis.module.js";
+import { S3Module } from "./core/s3/s3.module.js";
 import { AuthModule } from "./modules/auth/auth.module.js";
 import { PostModule } from "./modules/post/post.module.js";
 
 @Module({
-	imports: [CoreModule, PostModule, AuthModule],
+	imports: [ConfigModule, HealthChecksModule, JwtModule, PrismaModule, RateLimitModule, RedisModule, S3Module, PostModule, AuthModule],
 })
 export class AppModule {}
