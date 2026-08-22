3f08300082b8c129ac8e6043c4d9dd3fea48e8d2
o01011 <timetotime828@gmail.com>
2026-08-22 22:27:31 +0200

    fix(root): add linting types check scripts and update tsconfig references for all applications

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,34 +1,43 @@
 import type { Configuration } from "lint-staged";
 
 const config: Configuration = {
-	"*": [(): string => "npm run lint:clean:check", (): string => "npm run lint:fs:check"],
+	"*": [
+    (): string => "npm run lint:clean:check", (): string => "npm run lint:fs:check"
+  ],
 	"apps/backend-nestjs/**/*.ts": [
 		(): string => "npm run lint:format:check -w @web-monorepo/backend-nestjs",
 		(): string => "npm run lint:check -w @web-monorepo/backend-nestjs",
+		(): string => "npm run lint:types:check -w @web-monorepo/backend-nestjs",
 	],
 	"apps/backend-expressjs/**/*.ts": [
 		(): string => "npm run lint:format:check -w @web-monorepo/backend-expressjs",
 		(): string => "npm run lint:check -w @web-monorepo/backend-expressjs",
+		// (): string => "npm run lint:types:check -w @web-monorepo/backend-expressjs",
 	],
 	"apps/web-astro/**/*.{ts,tsx}": [
-		(): string => "npm run lint:format:check -w @web-monorepo/backend-expressjs",
-		(): string => "npm run lint:check -w @web-monorepo/backend-expressjs",
+		(): string => "npm run lint:format:check -w @web-monorepo/web-astro",
+		(): string => "npm run lint:check -w @web-monorepo/web-astro",
+		// (): string => "npm run lint:types:check -w @web-monorepo/web-astro",
 	],
 	"apps/web-tanstack/**/*.{ts,tsx}": [
-		(): string => "npm run lint:format:check -w @web-monorepo/backend-expressjs",
-		(): string => "npm run lint:check -w @web-monorepo/backend-expressjs",
+		(): string => "npm run lint:format:check -w @web-monorepo/web-tanstack",
+		(): string => "npm run lint:check -w @web-monorepo/web-tanstack",
+		// (): string => "npm run lint:types:check -w @web-monorepo/web-tanstack",
 	],
 	"apps/web-vite/**/*.{ts,tsx}": [
-		(): string => "npm run lint:format:check -w @web-monorepo/backend-expressjs",
-		(): string => "npm run lint:check -w @web-monorepo/backend-expressjs",
+		(): string => "npm run lint:format:check -w @web-monorepo/web-vite",
+		(): string => "npm run lint:check -w @web-monorepo/web-vite",
+		// (): string => "npm run lint:types:check -w @web-monorepo/web-vite",
 	],
 	"packages/shared/**/*.ts": [
-		(): string => "npm run lint:format:check -w @web-monorepo/backend-expressjs",
-		(): string => "npm run lint:check -w @web-monorepo/backend-expressjs",
+		(): string => "npm run lint:format:check -w @web-monorepo/shared",
+		(): string => "npm run lint:check -w @web-monorepo/shared",
+		// (): string => "npm run lint:types:check -w @web-monorepo/shared",
 	],
 	"packages/db/**/*.ts": [
-		(): string => "npm run lint:format:check -w @web-monorepo/backend-expressjs",
-		(): string => "npm run lint:check -w @web-monorepo/backend-expressjs",
+		(): string => "npm run lint:format:check -w @web-monorepo/db",
+		(): string => "npm run lint:check -w @web-monorepo/db",
+		(): string => "npm run lint:types:check -w @web-monorepo/db",
 	],
 };
 
