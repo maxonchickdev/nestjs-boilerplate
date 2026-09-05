5eb2e29bd95412905682a43ac8be932f9367e6a7
o01011 <timetotime828@gmail.com>
2026-09-05 13:37:17 +0200

    chore(root): update package.json to add branch-name-lint and modify prepush hook; adjust workspaces to include packages/db

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
 	$schema: "https://unpkg.com/knip@5/schema.json",
-	ignoreDependencies: ["tsconfig-paths", "pg"],
+	ignoreDependencies: ["tsconfig-paths", "pg", "branch-name-lint"],
 	ignoreMembers: ["DEVELOPMENT", "STAGING", "P2002", "P2025"],
 	workspaces: {
 		"apps/backend-expressjs": {},
